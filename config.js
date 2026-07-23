2f7a1beb4e73aee2f74e6865049529df2c05cd62
Babar086 <babarakram339@gmail.com>
2026-07-24 00:54:42 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+const BABAR_AI = {
+
+    name: "Babar AI Agent",
+
+    version: "1.0.0",
+
+    developer: "Babar Akram",
+
+    apiUrl: "http://127.0.0.1:8000/chat",
+
+    theme: "dark",
+
+    language: "en",
+
+    voice: true,
+
+    memory: true,
+
+    internet: false,
+
+    imageAnalysis: false,
+
+    fileReader: false
+
+};
